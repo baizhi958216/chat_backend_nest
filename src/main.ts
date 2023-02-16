3ff0f11932afdc2667f083de7e1d77ee7c9dc598
baizhi958216 <1475289190@qq.com>
2023-02-16 15:48:28 +0800

    Log server status
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.log(`🚀 服务端已开启 http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
