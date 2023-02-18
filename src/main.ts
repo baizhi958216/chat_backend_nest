8266944f2a94578add82fcc7d862c2cf76bc25d2
baizhi958216 <1475289190@qq.com>
2023-02-18 21:38:44 +0800

    Serve static files
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3000);
   Logger.log(`🚀 服务端已开启 http://localhost:${port}`, 'Bootstrap');
 }
