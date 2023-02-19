1f0a89ce49d77960028c3c6465019770527c6ca7
baizhi958216 <1475289190@qq.com>
2023-02-20 00:20:33 +0800

    Email: Send email with SMTP(qq)
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/email/service/email.service.ts
+++ src/email/service/email.service.ts
@@ -1,13 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Format } from 'src/utils/format.util';
+import { sendMail } from 'src/utils/mail.utils';
 import { CreateEmailDto } from '../dto/create-email.dto';
 
 @Injectable()
 export class EmailService {
-  createRegister(createEmailDto: CreateEmailDto) {
-    return createEmailDto.captcha;
+  async createRegister(createEmailDto: CreateEmailDto) {
+    const strArr = '1234567890qwertyuiopasdfghjklzxcvbnm'.split('');
+    let str = '';
+    for (let index = 0; index < 4; index++) {
+      str += strArr[Math.floor(Math.random() * strArr.length)];
+    }
+
+    const { response } = await sendMail(createEmailDto.captcha, str);
+    Logger.warn(
+      `向 ${createEmailDto.captcha} 发送注册验证码 ${str}, 状态 ${response}`,
+    );
+
+    return Format.getInstance().message(201, {
+      msg: createEmailDto,
+    });
   }
 
-  createLogin(createEmailDto: CreateEmailDto) {
-    return createEmailDto.captcha;
+  async createLogin(createEmailDto: CreateEmailDto) {
+    const strArr = '1234567890qwertyuiopasdfghjklzxcvbnm'.split('');
+    let str = '';
+    for (let index = 0; index < 4; index++) {
+      str += strArr[Math.floor(Math.random() * strArr.length)];
+    }
+
+    const { response } = await sendMail(createEmailDto.captcha, str);
+    Logger.warn(
+      `向 ${createEmailDto.captcha} 发送登录验证码 ${str}, 状态 ${response}`,
+    );
+
+    return Format.getInstance().message(201, {
+      msg: createEmailDto,
+    });
   }
 }
---
+++ src/utils/mail.utils.ts
@@ -0,0 +1,40 @@
+import { createTransport } from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+/**
+ *
+ * @param user 用户邮箱
+ * @param str 验证码
+ * @returns
+ */
+const sendMail = async (
+  user: string,
+  str: string,
+): Promise<SMTPTransport.SentMessageInfo> => {
+  const transport = createTransport({
+    host: 'smtp.qq.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_SMTP,
+    },
+  });
+  const mailOptions = {
+    from: '"夏" <958216793@qq.com>', // 发件人
+    to: `${user}`, // 收件人
+    subject: '欢迎注册立交桥', // 主题
+    text: `您的验证码是 ${str}`,
+    html: `<p align="center">您的验证码是 ${str}</p>`,
+  };
+  return new Promise((resolve, reject) => {
+    transport.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+};
+
+export { sendMail };
