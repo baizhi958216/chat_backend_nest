1ced3d9540b0db49ee3486409310a884ba2da6e0
baizhi958216 <1475289190@qq.com>
2023-03-30 16:30:08 +0800

    Redis: Sift docker environment
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/utils/redis.utils.ts
+++ src/utils/redis.utils.ts
@@ -1,7 +1,10 @@
 import { Logger } from '@nestjs/common';
 import { createClient, RedisClientType, RedisDefaultModules } from 'redis';
 
-const client: RedisClientType<RedisDefaultModules> = createClient();
+const client: RedisClientType<RedisDefaultModules> = process.env.REDIS_HOST == 'redis'?createClient({
+  url: 'redis://redis:6379',
+}):createClient();
+
 client.on('connect', () => {
   Logger.debug('Redis已连接');
 });
